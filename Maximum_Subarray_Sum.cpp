1b5bc5908d6681b2c25ca39989c3490cc776d924
Pratik Agarwal <83236605+pratik-31@users.noreply.github.com>
2022-06-16 15:42:50 +0530

    Day 2

---
+++ Maximum_Subarray_Sum.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h> 
+long long maxSubarraySum(int arr[], int n)
+{
+    /*
+        Don't write main().
+        Don't read input, it is passed as function argument.    
+        No need to print anything.
+        Taking input and printing output is handled automatically.
+    */
+    long long sum=0,ans=0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+        if(sum<0){
+            sum=0;
+        }
+        ans=max(ans,sum);
+    }
+    return ans;
+}
